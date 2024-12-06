f79381cb329e15cc9db518be474cc075f7530995
puspasri <puspa09111984@gmail.com>
2024-12-06 15:53:20 +0530

    Add files via upload

---
+++ dogbreed.py
@@ -0,0 +1,15 @@
+class dogbreed :
+
+    quality = 'cute'
+
+def __init__(self, name, color):
+    self.name = name 
+    self.color = color 
+
+
+Husky = dog('Husky', 'Brown')
+Beagle = dog('Beagle', 'black')
+
+
+print(husky.name , dog.color)
+print(beagle.name ,  beagle.color)
